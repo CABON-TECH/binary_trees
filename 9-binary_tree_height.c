56ecc799045464401891b22011a44077edd1e750
CABON-TECH <kishushubonifacembogho@gmail.com>
2023-02-28 12:35:23 -0800

    height

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_height - function that measures the height of a binary tree
+ *
+ * @tree: pointer to the root node of the tree to measure the height
+ *
+ * Return: height of tree. if tree == NULL, return 0
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+
+	/* find height of left and right subtrees recursively */
+	left_height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	/* find max(subtree_height) to get h of tree */
+	return (left_height > right_height ? left_height : right_height);
+}
